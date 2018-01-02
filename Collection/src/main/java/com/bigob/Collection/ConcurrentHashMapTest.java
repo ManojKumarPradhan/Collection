f1592cf115411eaaed2c4d4165a0b19736ad4ac9
ManojKumarPradhan <mkpmca123@gmail.com>
2018-01-03 03:32:07 +0530

    Concurrent Map

---
+++ Collection/src/main/java/com/bigob/Collection/ConcurrentHashMapTest.java
@@ -0,0 +1,11 @@
+package com.bigob.Collection;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentHashMapTest {
+
+	public static void main(String[] args) {
+		ConcurrentHashMap<Integer, String> concurrentHashMap=new ConcurrentHashMap<>();
+		System.out.println(concurrentHashMap);
+	}
+}
